8e597df565bb1ac738e070faa7d7fbc79cbfd8fd
zubair-saif <zubairsaif700@gmail.com>
2021-06-30 14:31:40 +0500

    style added

--- pages/post/first-post.js
+++ pages/post/first-post.js
@@ -1,11 +1,13 @@
-import Link from "next/link"
-import Head from "next/head"
+import Link from "next/link";
+import Head from "next/head";
+import styles from '../../styles/Home.module.css'
+
 export default function FirstPost(){
     return (
         <>
         <Head> <title>First Post</title></Head>
         <h1>First Post</h1>
-        <h2><Link href="/"><a>Back to Home</a></Link></h2>
+        <h2 className={styles.title}><Link href="/"><a>Back to Home</a></Link></h2>
         </>
     )
 }
